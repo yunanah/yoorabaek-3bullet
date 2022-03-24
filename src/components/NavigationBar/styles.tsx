ed8169c995f041cde275af3e5f5cb968b1d9136b
yunanah <bbyl6319@gmail.com>
2022-03-24 21:10:33 +0900

    💄 update ui style

--- src/components/NavigationBar/styles.tsx
+++ src/components/NavigationBar/styles.tsx
@@ -3,36 +3,75 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   justify-content: start;
-  align-items: center;
+  align-items: flex-start;
   flex-direction: column;
   color: #020202;
   background-color: #fff;
   position: fixed;
   left: 0;
   height: 100vh;
-  padding: 32px;
+  width: 25%;
+  padding: 50px 20px 20px 80px;
 
   @media screen and (max-width: 820px) {
     position: relative;
     /* flex-direction: row; */
     justify-content: space-between;
     align-items: flex-start;
+    width: 100vw;
     height: 300px;
+    padding: 30px;
   }
 `;
 export const Logo = styled.span`
-  margin-left: 30px;
-  font-size: 1.2em;
+  /* margin-left: 30px; */
+  font-size: 20px;
   text-align: center;
+
+  @media screen and (max-width: 820px) {
+    /* margin-left: 8px; */
+    font-size: 0.9em;
+  }
 `;
-export const Menu = styled.div`
+export const Menu = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-top: 80px;
+  /* margin-left: 30px; */
+
+  @media screen and (max-width: 820px) {
+    margin-top: 32px;
+    align-items: flex-end;
+    /* margin-left: 24px; */
+  }
+`;
+export const MenuItem = styled.li`
+  margin-bottom: 16px;
+  font-size: 20px;
+
+  @media screen and (max-width: 820px) {
+    font-size: 1.2em;
+  }
+`;
+
+export const LinkedMenu = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-end;
-  margin-top: 80px;
-  margin-left: 30px;
+  margin-top: 100px;
+  /* margin-left: 30px; */
 `;
-export const MenuItem = styled.div`
+export const LinkedItem = styled.a`
   margin-bottom: 16px;
-  font-size: 1em;
+  font-size: 20px;
+  text-decoration: underline;
+`;
+
+export const MobileLinkedMenu = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+  margin-top: 32px;
+  font-size: 18px;
 `;
